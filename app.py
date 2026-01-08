ccde030e626d67eab3ef32485e81da362af1d954
SAR-01 <syedabdulrehman429@gmail.com>
2026-01-08 15:57:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,17 @@ exp = st.number_input("Rider Experience (yrs)", min_value=0)
 if st.button("Predict Time"):
     features = np.array([[dist, 1, 1, 1, 1, prep, exp]])
     scaled_features = scaler.transform(features)
-    prediction = model.predict(scaled_features)
-    st.success(f"Estimated Time: {prediction[0]:.2f} Minutes")
+    prediction = model.predict(scaled_features)[0]  # Total minutes extract karein
+
+    # Calculation logic
+    hours = int(prediction // 60)
+    minutes = int(prediction % 60)
+    seconds = int((prediction * 60) % 60)
+
+    # Display Result
+    if hours > 0:
+        time_text = f"{hours} hrs {minutes} min {seconds} sec"
+    else:
+        time_text = f"{minutes} min {seconds} sec"
+        
+    st.success(f"⏳ Estimated Delivery Time: {time_text}")
